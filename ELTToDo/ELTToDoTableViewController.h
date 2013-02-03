a2d424a0f050063401a1e4082526e69412fa2722
Patrick McConnell <patrick@dogboystudios.com>
2013-02-03 11:46:58 -0500

    added tableview delegate methods to display todos

---
+++ ELTToDo/ELTToDoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELTToDoTableViewController.h
+//  ELTToDo
+//
+//  Created by Patrick McConnell on 2/3/13.
+//  Copyright (c) 2013 Patrick McConnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELTToDoTableViewController : UITableViewController
+
+@end
